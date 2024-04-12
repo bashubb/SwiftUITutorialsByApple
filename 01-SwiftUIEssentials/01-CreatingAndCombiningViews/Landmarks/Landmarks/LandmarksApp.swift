baa41282bcca80c6c8db4c7a51c02da6a472ac7d
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-04-12 12:16:49 +0200

    Creating ang combining views

---
+++ 01-SwiftUIEssentials/01-CreatingAndCombiningViews/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by HubertMac on 12/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
