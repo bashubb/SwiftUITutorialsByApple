7cc1a5223fc2cef58b3680e3e51c6b3967452cab
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-05-16 22:53:52 +0200

    Add watchOS Target

---
+++ 04-FrameworkIntegration/02-CreatingAWatchOSApp/StartingPoint/WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by HubertMac on 16/05/2024.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 04-FrameworkIntegration/02-CreatingAWatchOSApp/StartingPoint/WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by HubertMac on 16/05/2024.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
