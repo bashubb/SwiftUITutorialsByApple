de150eab1bd76d727d9375a811c923daa778d7cb
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-05-17 14:01:46 +0200

    Add a custom menu command

--- 04-FrameworkIntegration/03-CreatingAMacOSApp/StartingPoint/Landmarks/Views/Landmarks/LandmarkList.swift
+++ 04-FrameworkIntegration/03-CreatingAMacOSApp/StartingPoint/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -11,6 +11,7 @@ struct LandmarkList: View {
     @Environment(ModelData.self) var modelData
     @State private var showFavoritesOnly = false
     @State private var filter = FilterCategory.all
+    @State private var selectedLandmark: Landmark?
     
     enum FilterCategory: String, CaseIterable, Identifiable {
         case all = "All"
@@ -32,16 +33,23 @@ struct LandmarkList: View {
         let title = filter == .all ? "Landmarks" : filter.rawValue
         return showFavoritesOnly ? "Favorite \(title)" : title
     }
+    
+    var index: Int? {
+        modelData.landmarks.firstIndex(where: { $0.id == selectedLandmark?.id})
+    }
 
     var body: some View {
+        @Bindable var modelData = modelData
+        
         NavigationSplitView {
-            List {
+            List(selection: $selectedLandmark) {
                 ForEach(filteredLandmarks) { landmark in
                     NavigationLink {
                         LandmarkDetail(landmark: landmark)
                     } label: {
                         LandmarkRow(landmark: landmark)
                     }
+                    .tag(landmark)
                 }
             }
             .animation(.default, value: filteredLandmarks)
@@ -68,6 +76,7 @@ struct LandmarkList: View {
         } detail: {
             Text("Select a Landmark")
         }
+        .focusedValue(\.selectedLandmark, $modelData.landmarks[index ?? 0])
     }
 }
 
