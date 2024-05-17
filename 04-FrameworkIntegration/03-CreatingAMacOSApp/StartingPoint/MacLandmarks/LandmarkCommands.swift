132234f2c84ebcad6eeb53bec05fd99f70b3363c
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-05-17 13:43:52 +0200

    Add a built-in menu command

---
+++ 04-FrameworkIntegration/03-CreatingAMacOSApp/StartingPoint/MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,15 @@
+//
+//  LandmarkCommands.swift
+//  CreatingAwatchOSApp
+//
+//  Created by HubertMac on 17/05/2024.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
