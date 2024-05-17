fd525ac0a908aa6ea550672c5ee2256554b62f14
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-05-17 10:13:00 +0200

    resturcture

--- 04-FrameworkIntegration/03-CreatingAMacOSApp/StartingPoint/WatchLandmarks Watch App/NotificationController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NotificationController.swift
-//  WatchLandmarks Watch App
-//
-//  Created by HubertMac on 17/05/2024.
-//  Copyright © 2024 Apple. All rights reserved.
-//
-
-import Foundation
